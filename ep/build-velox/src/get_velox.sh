apache/incubator-gluten

Change velox branch

ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_01_12
+VELOX_REPO=https://github.com/philo-he/velox.git
+VELOX_BRANCH=2025_01_12-v1
 VELOX_HOME=""
 
 OS=`uname -s`

oap-project/gazelle-jni

ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2025_02_18
+VELOX_REPO=https://github.com/philo-he/velox.git
+VELOX_BRANCH=2025_02_13-v1
 VELOX_HOME=""
 
 OS=`uname -s`